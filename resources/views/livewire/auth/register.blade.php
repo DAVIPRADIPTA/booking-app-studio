7c9ac625e01b10fd0b1643d9d740e989da5057b2
mirsydfchrynto <mirsyadf1805@gmail.com>
2025-08-05 00:01:22 +0700

    Edit di fitur-baru

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,4 +1,4 @@
-{{-- <?php
+<?php
 
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
@@ -93,7 +93,7 @@ new #[Layout('components.layouts.auth')] class extends Component {
     </form>
 
     <div class="space-x-1 rtl:space-x-reverse text-center text-sm text-zinc-600 dark:text-zinc-400">
-        <span>{{ __('Already have an account?') }}</span>
+        {{-- <span>{{ __('Already have an account?') }}</span> --}}
         <flux:link :href="route('login')" wire:navigate>{{ __('Log in') }}</flux:link>
     </div>
-</div> --}}
+</div>
